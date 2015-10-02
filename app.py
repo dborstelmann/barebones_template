593664f24ab3b3988b76534a4c6b88ac8882f9c8
Daniel Borstelmann <dborstelmann@gmail.com>
2015-10-02 17:24:16 -0500

    database setup file and comments

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
+# When debug is true python server will update on file saves instead of needing
+# to re-run server
 if __name__ == '__main__':
     app.run(debug=True)
